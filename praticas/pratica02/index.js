cfe5bc248843a3967cad617a571380606321c480
Bruna Miyahara <bruna.m.miyahara@gmail.com>
2024-03-01 19:41:51 -0300

    Pratica 02

--- praticas/pratica02/index.js
+++ praticas/pratica02/index.js
@@ -10,28 +10,50 @@ function menu() {
     console.log("6. Sair");
 }
 
+function nome() {
+    const nome = readline.question("nome: ");
+    return nome
+}
+
+function email() {
+    const email = readline.question("email: ");
+    return email
+}
+
+function telefone() {
+    const telefone = readline.question("telefone: ");
+    return telefone;
+}
+    
 function escolherOpcao(opcao) {
-    let nome = readline.question("nome: ");
-    let email = readline.question("email: ");
-    let telefone = readline.question("telefone: ");
     switch(opcao) {
         case '1' :
-        contatoControlador.adicionarContato(nome, email, telefone);
-        break;
-        case '2' : 
-        contatoControlador.listarContatos();
-        break;
+            const nome1 = nome();
+            const email1 = email();
+            const telefone1 = telefone();
+            contatoControlador.adicionarContato(nome1, email1, telefone1);
+            break;
+        case '2' :
+            contatoControlador.listarContatos();
+            break;
         case '3' :
-        contatoControlador.buscarContato(nome);
-        break;
+            const nome3 = nome();
+            contatoControlador.buscarContato(nome3);
+            break;
         case '4' :
-        contatoControlador.atualizarContato(nome, email, telefone);
-        break;
+            const nome4 = nome();
+            const email4 = email();
+            const telefone4 = telefone();
+            contatoControlador.atualizarContato(nome4, email4, telefone4);
+            break;
         case '5' :
-        contatoControlador.removerContato(nome);
-        break;
-        case '6' : process.exit(0);
-        default: console.log('Opcao Invalida');
+            const nome5 = nome();
+            contatoControlador.removerContato(nome5);
+            break;
+        case '6' :
+            process.exit(0);
+        default: 
+            console.log('Opcao Inválida');
     }
 }
 
