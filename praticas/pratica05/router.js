a9200859edac44a5ee4a921cdc9b637314bacfee
BrunaMiyahara <bruna.m.miyahara@gmail.com>
2024-03-22 08:28:20 -0300

    Pratica05

--- praticas/pratica05/router.js
+++ praticas/pratica05/router.js
@@ -4,4 +4,13 @@ const router = express.Router();
 
 const produtos = [];
 
+router.get("/produtos/:produtoId", function(req, res) {
+    const encontrado = produtos.find(item => item.id == req.params.produtoId);
+    if (encontrado) {
+      res.json(encontrado);  //200 e JSON
+    } else {
+      res.status(404).json({msg: "Produto nao encontrado"});
+    }
+});
+
 module.exports = router;
